4f9b9828cef4a0838d5150e9b31306911b9f7112
Bernard <bernardd.lego274@gmail.com>
2025-04-01 09:32:34 -0300

    segundo exercicio realizado com sucesso

---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+n1 = int(input("digite o primeiro valor: "))
+n2 = int(input("gitige o segundo valor: "))
+soma = (n1 + n2)
+subtraçao = (n1 - n2)
+multiplicação = (n1 * n2)
+divisao = (n1 / n2)
+
+print(f" a soma do valor é: {soma}, "
+      f"a subtração é: {subtraçao}, "
+      f"a multiplicação é: {multiplicação} e "
+      f"a divisão é: {divisao}")
