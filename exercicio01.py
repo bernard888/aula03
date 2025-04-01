ccb86d14afa21d4a8749b39a392953fd847a2b4c
Bernard <bernardd.lego274@gmail.com>
2025-04-01 08:52:54 -0300

    task de criar um log

---
+++ exercicio01.py
@@ -0,0 +1,7 @@
+nome = input("Digite o seu nome completo: ")
+idade = int(input("digite a sua idade: "))
+salario = float(input("digite seu salario: "))
+
+print(" ")
+
+print(f"Olá, {nome}! Sua idade é {idade} e seu salário é de {salario}")
